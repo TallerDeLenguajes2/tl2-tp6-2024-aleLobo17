6769521e828b555f0cc7756c12e9cd9fd1c49dd0
Lobo <lobo.rodrigoalejandro.2020@gmail.com>
2024-11-08 17:46:07 -0300

    -

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp6_2024_aleLobo17.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
